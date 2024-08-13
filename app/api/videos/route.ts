670c16fd64840faa38f8bc5c86923cc13cc3f737
Ramanath K P <ramnathnaik447@gmail.com>
2024-08-13 22:06:09 +0530

    add prsima and neon db

---
+++ app/api/videos/route.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  try {
+    // Now you can use the prisma client instance here
+    const videos = await prisma.video.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+    return NextResponse.json(videos);
+  } catch (error) {
+    //Handle errors
+    console.log(error);
+    return NextResponse.json(
+      { error: "An error occurred while getting videos" },
+      {
+        status: 500,
+      }
+    );
+  }
+}
